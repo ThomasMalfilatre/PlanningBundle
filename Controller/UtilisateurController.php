0f25ec548cedd9b46571c42c16b24abfa81a9592
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-03-18 09:11:20 +0100

    ajout de l'entitée Activity et du controller Utilisateur

---
+++ Controller/UtilisateurController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Iut\PlanningBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class UtilisateurController extends Controller
+{
+    /**
+     * @Route("/add")
+     * @Template()
+     */
+    public function addAction()
+    {
+    }
+
+    /**
+     * @Route("/id")
+     * @Template()
+     */
+    public function showAction()
+    {
+    }
+
+}
