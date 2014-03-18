014073aea2d3c63970c8257e0142c4208cb25837
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-03-18 14:55:58 +0100

    ajout des entitées des vus et des formulaires

---
+++ Form/UsersType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Iut\PlanningBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UsersType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prénom')
+            ->add('pseudo')
+            ->add('motDePasse')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Iut\PlanningBundle\Entity\Users'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'iut_planningbundle_users';
+    }
+}
