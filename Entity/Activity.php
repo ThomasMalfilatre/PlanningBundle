d47c03e58e203eaf41c7b31ed21e00077c3faf3d
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-03-18 09:11:49 +0100

    ajout de l'entitée Activity et du controller Utilisateur

---
+++ Entity/Activity.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Iut\PlanningBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Activity
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Activity
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="activite", type="string", length=30)
+     */
+    private $activite;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set activite
+     *
+     * @param string $activite
+     * @return Activity
+     */
+    public function setActivite($activite)
+    {
+        $this->activite = $activite;
+
+        return $this;
+    }
+
+    /**
+     * Get activite
+     *
+     * @return string 
+     */
+    public function getActivite()
+    {
+        return $this->activite;
+    }
+}
