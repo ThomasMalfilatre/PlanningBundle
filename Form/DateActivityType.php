8b7f2b8401c51ae7852cac0fc2b4e8cf64afa1f7
Thomas Malfilatre <thomas.malfilatre@etu.univ-orleans.fr>
2014-03-27 14:53:51 +0100

    modif entité dateActivity + création formulaire ajout activité

---
+++ Form/DateActivityType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Iut\PlanningBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DateActivityType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('activite')
+            ->add('pseudo')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Iut\PlanningBundle\Entity\DateActivity'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'iut_planningbundle_dateactivity';
+    }
+}
